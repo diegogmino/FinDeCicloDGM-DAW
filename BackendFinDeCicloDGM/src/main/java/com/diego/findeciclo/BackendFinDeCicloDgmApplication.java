250a1ba2196d8774e05710c9077d0b97c5d4bf1e
Diego <diego.garcia.mino@gmail.com>
2022-04-16 15:35:58 +0200

    Vista del administrador iniciada.

--- BackendFinDeCicloDGM/src/main/java/com/diego/findeciclo/BackendFinDeCicloDgmApplication.java
+++ BackendFinDeCicloDGM/src/main/java/com/diego/findeciclo/BackendFinDeCicloDgmApplication.java
@@ -2,12 +2,22 @@ package com.diego.findeciclo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
-public class BackendFinDeCicloDgmApplication {
+public class BackendFinDeCicloDgmApplication extends WebMvcConfigurationSupport {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendFinDeCicloDgmApplication.class, args);
 	}
+
+	 // El paquete de guía de ruta para configurar archivos de recursos estáticos aquí se importa directamente de forma predeterminada.
+	 @Override
+	 protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		 registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
+		 super.addResourceHandlers(registry);
+	 }
 
 }
--- BackendFinDeCicloDGM/src/main/java/com/diego/findeciclo/controller/DirectoresController.java
+++ BackendFinDeCicloDGM/src/main/java/com/diego/findeciclo/controller/DirectoresController.java
@@ -1,22 +1,16 @@
 package com.diego.findeciclo.controller;
 
-import com.diego.findeciclo.service.IPeliculaService;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value = "/admin/peliculas")
-public class PeliculasController {
+@RequestMapping(value = "/admin/directores")
+public class DirectoresController {
     
-    @Autowired
-	private IPeliculaService peliculaService;
-
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String mostrarPeliculas() {
-        return "peliculas/listadoPeliculas";
+        return "directores/listadoDirectores";
     }
 
 }

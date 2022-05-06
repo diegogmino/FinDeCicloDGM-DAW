bcc134bbb255c128eeada112946643d3dd84fedb
Diego <diego.garcia.mino@gmail.com>
2022-05-06 14:26:20 +0200

    Corrección de un error al borrar un perfil

--- BackendFinDeCicloDGM/src/main/java/com/diego/findeciclo/controller/PerfilController.java
+++ BackendFinDeCicloDGM/src/main/java/com/diego/findeciclo/controller/PerfilController.java
@@ -3,7 +3,9 @@ package com.diego.findeciclo.controller;
 import java.util.List;
 
 import com.diego.findeciclo.model.Perfil;
+import com.diego.findeciclo.model.Usuario;
 import com.diego.findeciclo.service.IPerfilService;
+import com.diego.findeciclo.service.IUsuarioService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,6 +21,9 @@ public class PerfilController {
     
     @Autowired
     private IPerfilService perfilService;
+
+    @Autowired
+    private IUsuarioService usuarioService;
 
     // MÉTODOS GET
     @RequestMapping(value = "/index", method = RequestMethod.GET)
@@ -49,11 +54,30 @@ public class PerfilController {
     }
 
     @RequestMapping(value = "/eliminar/{id}", method = RequestMethod.GET)
-    public String eliminar(@PathVariable int id) {
+    public String eliminar(@PathVariable int id, Model model) {
 
-        perfilService.eliminarPerfil(id);
-        return "redirect:/admin/perfiles/index";
+        List<Usuario> usuarios = usuarioService.buscarTodos();
+        Perfil perfil = perfilService.buscarPerfil(id);
 
+        boolean tieneUsuario = false;
+        for(Usuario usuario : usuarios) {
+            if(usuario.getPerfil().equals(perfil)) {
+                tieneUsuario = true;
+                break;
+            }
+        }
+
+        if(!tieneUsuario) {
+            perfilService.eliminarPerfil(id);
+            return "redirect:/admin/perfiles/index";
+        }
+
+        List<Perfil> perfiles = perfilService.buscarTodos();
+
+        model.addAttribute("alerta", true);
+        model.addAttribute("perfiles", perfiles);
+        model.addAttribute("perfil", perfil);
+        return "perfiles/listadoPerfiles";
     }
 
     // MÉTODOS POST
--- BackendFinDeCicloDGM/src/main/java/com/diego/findeciclo/model/Perfil.java
+++ BackendFinDeCicloDGM/src/main/java/com/diego/findeciclo/model/Perfil.java
@@ -39,5 +39,36 @@ public class Perfil {
 		this.perfil = perfil;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((perfil == null) ? 0 : perfil.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Perfil other = (Perfil) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (perfil == null) {
+			if (other.perfil != null)
+				return false;
+		} else if (!perfil.equals(other.perfil))
+			return false;
+		return true;
+	}
+
 	
 }
